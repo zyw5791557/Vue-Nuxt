a7e6084dfdb043d104cb7d8d7ceaf8f1caa5e319
Amo <xiaomozyw@sina.com>
2017-11-09 18:22:30 +0800

    initial commit
    Signed-off-by: Amo <xiaomozyw@sina.com>

---
+++ nuxt.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+	/*
+	** Headers of the page
+	*/
+	head: {
+		title: 'vue-nuxt',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: 'Nuxt.js project' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+		]
+	},
+	/*
+	** Customize the progress bar color
+	*/
+	loading: { color: '#3B8070' },
+	/*
+	** Build configuration
+	*/
+	build: {
+		/*
+		** Run ESLint on save
+		*/
+		extend(config, { isDev, isClient }) {
+			// ...
+			config.module.rules.push({
+				enforce: 'pre',
+				test: /\.scss$/,
+				loader: 'vue-style-loader!css-loader!sass-loader',
+				exclude: /(node_modules)/
+			})
+		}
+	}
+}
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="app">
+		<h1 class="xixi">
+			测试
+		</h1>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+	#app {
+		.xixi {
+			color: red;
+		}
+	}
+</style>
